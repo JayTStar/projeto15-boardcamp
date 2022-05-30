6b636fb5abafb2396a0a3fb1dff76557d9802e5d
Jorge Tadeu <jsbatalha@gmail.com>
2022-05-30 15:39:40 -0300

    deploy

--- src/Routers/gamesRoutes.js
+++ src/Routers/gamesRoutes.js
@@ -4,7 +4,7 @@ const gamesRouter = Router();
 
 import {getGames, postGames} from "../Controllers/gamesController.js";
 import {gamesSchema} from "../Schemas/gamesSchema.js";
-import {validateSchema} from "../Middlewares/joiValidation.js";
+import validateSchema from "../Middlewares/joiValidation.js";
 
 gamesRouter.get("/games", getGames);
 gamesRouter.post("/games", (req,res,next) => {validateSchema(req,res,next,gamesSchema)}, postGames);
