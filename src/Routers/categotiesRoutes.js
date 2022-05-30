5f6546342839bbd1f8fa9d3aea8f4d7cbb03d7a1
Jorge Tadeu <jsbatalha@gmail.com>
2022-05-30 15:38:36 -0300

    deploy

--- src/Routers/categotiesRoutes.js
+++ src/Routers/categotiesRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 
 import { getCategories, postCategories } from "../Controllers/categoriesController.js";
-import { validateSchema } from "../Middlewares/joiValidation.js"
+import  validateSchema  from "../Middlewares/joiValidation.js"
 import { categorySchema } from "../Schemas/categoriesSchema.js";
 
 const categoriesRouter = Router();
 
 categoriesRouter.get("/categories", getCategories);
-categoriesRouter.post("/categories", (req,res,next) => {joiValidation(req,res,next, categorySchema)}, postCategories);
+categoriesRouter.post("/categories", (req,res,next) => {validateSchema(req,res,next, categorySchema)}, postCategories);
 
 export default categoriesRouter;
