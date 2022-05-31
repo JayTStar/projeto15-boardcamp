921f9f8db2b72f32740815392c0fd1f45e79961b
Jorge Tadeu Soares Batalha <jsbatalha@gmail.com>
2022-05-31 15:18:46 -0300

    Ajeitando o Deploy

--- src/data.js
+++ src/data.js
@@ -1,17 +1,13 @@
 import pg from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const {Pool} = pg;
-
-const databaseConfig = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+if(process.env.MODE === "PROD") {
+  db.ssl = {
+    rejectUnauthorized: false
+  }
 }
-
-const connection = new Pool({databaseConfig});
-
-export default connection;
+export default db;
